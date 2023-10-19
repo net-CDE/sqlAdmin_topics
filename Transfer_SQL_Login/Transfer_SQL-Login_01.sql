146fb1d4d7a1a82e6dd3fd97ffb38e84296cb56b
net-CDE <alexanderkarl@gmx.net>
2023-10-19 19:04:33 +0200

    reformat Transfer_SQL-Login_01.sql

--- Transfer_SQL_Login/Transfer_SQL-Login_01.sql
+++ Transfer_SQL_Login/Transfer_SQL-Login_01.sql
@@ -1,4 +1,4 @@
-ALTER Proc spTool_hexadecimal
+CREATE Proc spTool_hexadecimal
             @binValue  varbinary(256)  
           , @hexValue  varchar(514)  OUTPUT
 AS
@@ -7,7 +7,7 @@ BEGIN
 DECLARE @charvalue  varchar(514)
       , @ii         int
       , @lenght     int
-	  , @hexstring  char(16)
+      , @hexstring  char(16)
 
 SELECT  @charvalue = '0x'
 SELECT  @ii        = 1
@@ -16,25 +16,23 @@ SELECT  @hexstring = '0123456789ABCDEF'
 
 WHILE ( @ii <= @lenght )
 BEGIN
-	DECLARE @tempint   int
-	      , @firstint  int
-		  , @secondint int
+    DECLARE @tempint   int
+          , @firstint  int
+          , @secondint int
 
-	SELECT  @tempint   = CONVERT( int, Substring( @binvalue, @ii, 1 ))
-	SELECT  @firstint  = FLOOR( @tempint/ 16 )
-	SELECT  @secondint = @tempint - ( @firstint * 16 )
+    SELECT  @tempint   = CONVERT( int, Substring( @binvalue, @ii, 1 ))
+    SELECT  @firstint  = FLOOR( @tempint/ 16 )
+    SELECT  @secondint = @tempint - ( @firstint * 16 )
 
-	SELECT  @charvalue = @charvalue 
-	                   + SubString( @hexstring , @firstint + 1, 1 )
-					   + SubString( @hexString , @secondint + 1, 1 )
+    SELECT  @charvalue = @charvalue 
+                       + SubString( @hexstring , @firstint  + 1, 1 )
+                       + SubString( @hexString , @secondint + 1, 1 )
 
-	----
-	Set @ii = @ii + 1
+    ----
+    Set @ii = @ii + 1
 END  -- end WHILE
 
 -- Return
 SELECT @hexValue = @charvalue
 
 END  -- end Proc
-
-
