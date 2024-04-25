ed7cfe1baaa64488394650a0b333adb59401ee27
net-CDE <alexanderkarl@gmx.net>
2024-04-25 14:25:28 +0200

    tsql__vw_Logins_cross_ServerRoles  -- reformated

--- tsql_4migration/tsql__vw_Logins_cross_ServerRoles.sql
+++ tsql_4migration/tsql__vw_Logins_cross_ServerRoles.sql
@@ -10,10 +10,10 @@ SELECT @@SERVERNAME                                         as  ServerName
      , MAX(CASE WHEN rm.role_principal_id =  8 THEN 1 END)  as  diskadmin      
      , MAX(CASE WHEN rm.role_principal_id =  9 THEN 1 END)  as  dbcreator      
      , MAX(CASE WHEN rm.role_principal_id = 10 THEN 1 END)  as  bulkadmin      
-FROM  sys.server_principals   AS sr            
-JOIN  sys.server_role_members AS rm            
-ON    sr.principal_id = rm.role_principal_id   
-JOIN  sys.server_principals AS sp              
-ON    rm.member_principal_id = sp.principal_id 
-GROUP BY  sp.name                              
+FROM   sys.server_principals   AS sr            
+JOIN   sys.server_role_members AS rm            
+ON     sr.principal_id = rm.role_principal_id   
+JOIN   sys.server_principals AS sp              
+ON     rm.member_principal_id = sp.principal_id 
+GROUP  BY  sp.name                              
 --  end  VIEW                                  
