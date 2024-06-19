d53cadf3b9679fd0c4d2cf5d1a77b300dbd0f081
sql-fabrik <git4schulung@sql-fabrik.de>
2024-06-19 15:23:31 +0200

    vw_JobHistory  updated

--- tsql_getSQLAgendDetails/tsql__vw_JobHistory.sql
+++ tsql_getSQLAgendDetails/tsql__vw_JobHistory.sql
@@ -1,18 +1,22 @@
 CREATE VIEW [audit].[vw_JobHistory]
 AS 
 
-SELECT @@SERVERNAME    as 'Servername'  
-     , J.name          as 'Jobname'     
+SELECT @@SERVERNAME   as 'Servername'  
+     , J.name         as 'Jobname'
      , H.*
+     , CASE	                                           
+          WHEN  H.run_status  = 0  THEN  'Failed'      
+          WHEN  H.run_status  = 1  THEN  'Succeeded'   
+          WHEN  H.run_status  = 2  THEN  'Retry'       
+          WHEN  H.run_status  = 3  THEN  'Canceled'    
+          WHEN  H.run_status  = 4  THEN  'In Progress' 
+          ELSE                            NULL         
+       END            as 'run_status_desc'             
 FROM   msdb.dbo.sysjobs J         
 JOIN   msdb.dbo.sysjobhistory H   
 ON     J.job_id = H.job_id        
 WHERE  J.name like 'job_proj%'    
   and  H.run_status <> 1          
-                        --  0 = Failed       
-                        --  1 = Succeeded    
-                        --  2 = Retry        
-                        --  3 = Canceled     
-                        --  4 = In Progress  
--- end view 
+
+-- end view
 GO
