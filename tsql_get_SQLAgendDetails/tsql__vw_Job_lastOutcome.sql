100785b45f5e7c10927043de46cc8bb2ca506637
sql-fabrik <git4schulung@sql-fabrik.de>
2024-06-20 16:13:33 +0200

    'failed Stepname'  added

--- tsql_get_SQLAgendDetails/tsql__vw_Job_lastOutcome.sql
+++ tsql_get_SQLAgendDetails/tsql__vw_Job_lastOutcome.sql
@@ -1,11 +1,11 @@
-CREATE VIEW audit.vw_Job_lastOutcome
+CREATE VIEW [audit].[vw_Job_lastOutcome]
 AS
 
 WITH
 SubQuery as
 (
 SELECT J.job_id 
-     , J.name               as 'Jobname'         
+     , J.name               as 'Jobname'     
      , max(H.instance_id)   as 'max_instance_id' 
 FROM   msdb.dbo.sysjobs J         
 JOIN   msdb.dbo.sysjobhistory H   
@@ -13,16 +13,20 @@ ON     J.job_id = H.job_id
 GROUP  by 
        J.job_id
      , J.name
-)
-SELECT ROW_NUMBER() over (Order by J.Jobname)  as 'rowNr'
-     , @@SERVERNAME   as 'Servername'  
-     , J.Jobname                       
-     , H.instance_id                   
-     , H.run_date                      
-     , H.run_time                      
---   , CAST(H.run_date as bigint) * 1000000                     
---   + CAST(H.run_time as bigint)             as 'run_date_time'
-     , H.step_id   
+) 
+SELECT distinct
+       ROW_NUMBER() over 
+       (Order by @@SERVERNAME, J.Jobname)      as 'rowNr'       
+     , @@SERVERNAME                            as 'Servername'  
+     , J.Jobname    
+     , H.instance_id    
+     , H.run_date   
+     , H.run_time   
+     , COALESCE(D.step_name, '')               as 'Stepname'       
+     , CASE                                                        
+          WHEN H.run_status <> 1   THEN  CAST(D.step_id as char(3))
+          ELSE                           ''                        
+       END                                     as 'StepId'         
      , H.run_status
      , CASE	                                           
           WHEN  H.run_status  = 0  THEN  'Failed'      
@@ -31,11 +35,18 @@ SELECT ROW_NUMBER() over (Order by J.Jobname)  as 'rowNr'
           WHEN  H.run_status  = 3  THEN  'Canceled'    
           WHEN  H.run_status  = 4  THEN  'In Progress' 
           ELSE                            NULL         
-       END            as 'run_status_desc'             
-FROM   SubQuery J                       
-JOIN   msdb.dbo.sysjobhistory H         
+       END            as 'run_status_desc'  
+FROM   SubQuery J
+join   msdb.dbo.sysjobhistory H 
 ON     J.job_id          = H.job_id     
   and  J.max_instance_id = H.instance_id
+left   join   
+       msdb.dbo.sysjobhistory D  -- Step-Details
+ON     D.job_id     =  H.job_id
+ and   D.run_date   >= H.run_date
+ and   D.run_time   >= H.run_time
+ and   D.run_status =  0
+WHERE  1=1
 
 -- end view
 GO
